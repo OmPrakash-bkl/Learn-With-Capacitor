cfc3dc13d86efc1f3de9972089728d3e8126e03d
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2021-08-03 19:00:41 +0530

    Zceppa Pages

---
+++ index.js
@@ -0,0 +1,6 @@
+function showborder1() {
+    document.getElementsByClassName("input1").style.backgroundColor="red";
+}
+function showborder2() {
+    document.getElementsByClassName("input2").style.border="0.18rem solid rgb(0, 172, 172)";
+}
