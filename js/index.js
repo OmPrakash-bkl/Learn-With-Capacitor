2de837b6f1c5f157a6a5634cf2d9a838571978f0
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2021-08-05 19:50:50 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
+function showPopUpNav() {
+if(document.getElementById("nav_container").className=="color") {
+    document.getElementById("nav_container").className="white";
+    document.getElementById("popup_navbar").style.top = "6rem";
+    document.getElementById("popup_navbar").style.transition="0.3s";
+    document.getElementById("burger_btn").src = "./images/letter-x.png";
 
+} else {
+    document.getElementById("nav_container").className="color";
+    document.getElementById("popup_navbar").style.top = "-12rem";
+    document.getElementById("popup_navbar").style.transition="-0.3s";
+    document.getElementById("burger_btn").src = "./images/menu.png";
+} 
+
+}
+
+
+
+window.addEventListener("resize", function() {
+    if(document.documentElement.clientWidth > "1000") {
+        document.getElementById("popup_navbar").style.top = "-12rem";
+        document.getElementById("popup_navbar").style.transition="-0.3s";
+        document.getElementById("nav_container").className="color";
+    document.getElementById("burger_btn").src = "./images/menu.png";
+       
+    }
+   
+   
+});
