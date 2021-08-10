9434931d9afbd65aa9d045894685e2ca69b2e0c9
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2021-08-10 18:43:32 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -24,6 +24,48 @@ window.addEventListener("resize", function() {
     document.getElementById("burger_btn").src = "./images/menu.png";
        
     }
-   
-   
 });
+
+window.addEventListener("scroll", function() {
+ if(window.pageYOffset > 50) {
+   document.getElementById("nav_container").className="active";
+ document.getElementById("fir_text").className="active1";
+ document.getElementById("sec_text").className="active1";
+ document.getElementById("thi_text").className="active1";
+ document.getElementById("logo").style.display="none";
+ document.getElementById("logo1").style.display="block";
+ }
+ else {
+    document.getElementById("nav_container").className="color";
+ document.getElementById("fir_text").className="for_diff_color";
+ document.getElementById("sec_text").className="for_diff_color";
+ document.getElementById("thi_text").className="for_diff_color";
+ document.getElementById("logo").style.display="block";
+ document.getElementById("logo1").style.display="none";
+ }
+if(document.documentElement.clientWidth <= "1000") {
+    document.getElementById("logo").style.display="none";
+    document.getElementById("logo1").style.display="none";
+}
+
+});
+
+window.addEventListener("resize", function(){
+    if(document.documentElement.clientWidth <= "1000") {
+        document.getElementById("logo").style.display="none";
+ document.getElementById("logo1").style.display="none";
+    } 
+   if(window.pageYOffset > 50) {
+    document.getElementById("nav_container").className="active";
+    document.getElementById("fir_text").className="active1";
+    document.getElementById("sec_text").className="active1";
+    document.getElementById("thi_text").className="active1";
+   
+   }else {
+    document.getElementById("nav_container").className="color";
+ document.getElementById("fir_text").className="for_diff_color";
+ document.getElementById("sec_text").className="for_diff_color";
+ document.getElementById("thi_text").className="for_diff_color";
+ 
+ }
+})
